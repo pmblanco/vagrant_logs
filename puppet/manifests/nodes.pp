8706598d887faee838a3e2cbecdab1dc328d7309
Pedro Miguel Blanco Gómez <pmblanco@ardemans.com>
2014-03-15 11:48:50 +0100

    Pruebas con environments de puppet: Se actualiza version de modulo de puppet y se configuran pruebas en el nodes.pp

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,13 +1,37 @@
 node 'puppet1.vag.ardemans.int' {
 
   class { 'roles::common':
-    stage  => 'pre',
+    stage          => 'pre',
   }
   
   class { 'puppet::master':
+    reports        => 'puppetdb',
+	dns_alt_names  => 'perico.palotes.com,tomaya.ya.yaa',
   }
   
-  class { 'puppet::agent':
+  class { 'puppet::configmain':
+    certname       => 'prueba-cert-name-2',
+    masterserver   => 'prueba',
+  }
+  
+  puppet::environment { 'dev':
+    manifestdir    => '/etc/puppet/environments/dev/manifests',
+	manifest       => 'site.pp',
+	modulepath     => '/etc/puppet/environments/dev',
+  }
+
+  puppet::environment { 'pre':
+    manifestdir    => '/etc/puppet/environments/pre/manifests',
+	manifest       => 'site.pp',
+	modulepath     => '/etc/puppet/environments/pre',
+  }
+
+  puppet::environment { 'int':
+    manifestdir    => '/etc/puppet/environments/int/manifests',
+	manifest       => 'site.pp',
+	modulepath     => '/etc/puppet/environments/int',
   }
  
+  
+
 }
