d7210fbde6e1ded1f032cc274f832c7149344bd1
Pedro Miguel Blanco <pmblanco@prisadigital.com>
2014-04-01 20:05:40 +0200

    Nueva configuracion para la creación del vhost de apache para puppetboard. Además se unifica el donde se guardan los datos de SSL de puppet agent

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -44,7 +44,7 @@ node 'dashboard1.vag.ardemans.int' {
     masterserver   => 'puppet1.vag.ardemans.int',
 	service_status => 'stopped',
 	rundir         => '/var/run/puppet',
-	ssldir         => '/etc/puppet/ssl',
+	ssldir         => '/var/lib/puppet/ssl',
   }
   
   package { 'python-pip':
@@ -58,12 +58,29 @@ node 'dashboard1.vag.ardemans.int' {
   apache::vhost { 'puppetboard':
     port                        => '80',
     docroot                     => '/var/www/puppetboard',
+	aliases                     => [
+	  { alias   => '/static',
+	    path    => '/usr/lib/python2.6/site-packages/puppetboard/static',
+	  }
+	],
+	directories                 => [
+	  {
+	    path             => '/var/www/puppetboard',
+		options          => 'Indexes FollowSymLinks MultiViews'
+	  },
+	  {
+	    path             => '/usr/lib/python2.6/site-packages/puppetboard',
+		options          => 'Indexes',
+		custom_fragment  => "Require all granted\n    WSGIProcessGroup puppetboard\n    WSGIApplicationGroup %{GLOBAL}",
+	  },
+	],
     wsgi_daemon_process         => 'puppetboard',
     wsgi_daemon_process_options =>
       { processes    => '2', 
-        threads      => '15', 
+        threads      => '15',
+		user         => 'apache',
+		group        => 'apache',
        },
-    wsgi_process_group          => 'puppetboard',
     wsgi_script_aliases         => { '/' => '/var/www/puppetboard/wsgi.py' },
   }  
   
