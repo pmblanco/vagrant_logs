8377cb78509a2606aad81ad114426161f18d324d
Pedro Miguel Blanco Gómez <pmblanco@ardemans.com>
2014-05-05 17:44:33 +0200

    Script de repuppet

--- puppet/modules/roles/manifests/common.pp
+++ puppet/modules/roles/manifests/common.pp
@@ -17,4 +17,12 @@ class roles::common {
   class { 'puppet::repo':
   }
   
+  file { '/usr/local/sbin/repuppet':
+    ensure   => file,
+	owner    => 'root',
+	group    => 'root',
+	mode     => '0755',
+	content  => "#!/bin/bash\npuppet apply --debug --modulepath=/vagrant/puppet/modules/ --hiera_config=/vagrant/hiera.yaml /vagrant/puppet/manifests/site.pp",
+  }  
+  
 }
